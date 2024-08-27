e94bf9ab3ab4dfead41c459995da4d94d1645085
Marrtinerz <martins@siftinteractive.com>
2024-08-27 05:45:02 +0100

    completed new time series project

---
+++ Predictive Modeling of Renewable Energy Generation Using Time Series and Regression/app/model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import pickle
+
+#defining the base directory here
+base = '\app'
+reg = xgb.XGBRegressor()
+#loading the pickle file
+model = pickle.load(open(f'{base}/model2.pkl', "rb"))
+#model = reg.load_model(f'{base}/model2.json')
+
+def create_f(df):
+    df = df.copy()
+    df['hour'] = df.index.hour
+    df['minute'] = df.index.minute
+    df['day'] = df.index.day
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    df['dayofweek'] = df.index.dayofweek
+    df['dayofyear'] = df.index.dayofyear
+    df['weekofyear'] = df.index.isocalendar().week
+    
+    return df
+
+
+def create_pd(fr,to):
+    new = pd.date_range(fr+' 12:00:00+00:00',to+' 12:00:00+00:00', freq='10min')
+    new = pd.DataFrame(index=new)
+    new = create_f(new)
+    return new
